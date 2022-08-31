28c3e0c681ed67abb761524ecf55f4d3b2a7d17b
Mariana Calado <mariana_melo30@hotmail.com>
2022-08-30 23:48:02 -0300

    add pastas e add atividade string

---
+++ logica/modulo-01/string.py
@@ -0,0 +1,11 @@
+frase = "hello world"
+frase_maiusc = frase.upper()
+print(frase_maiusc)
+
+frase = "hello world"
+frase_minusc = frase.lower()
+print(frase_minusc)
+
+frase = "hello world"
+frase_qtdCaracteres = len(frase.split())
+print(frase_qtdCaracteres)
